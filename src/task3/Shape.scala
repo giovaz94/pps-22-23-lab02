1cc4e1ba7b8e0a222cca84c2b85dd653e6448aeb
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-03-15 19:05:01 +0100

    Add contains function

--- src/task3/Shape.scala
+++ src/task3/Shape.scala
@@ -1,5 +1,7 @@
 package task3
 
+import scala.annotation.tailrec
+
 enum Shape :
   case Rectangle(p1: Point2D, p2: Point2D)
   case Circle(p: Point2D, r: Double)
@@ -9,8 +11,18 @@ enum Shape :
 object Shape extends App:
   def perimeter(s: Shape): Double = s match
     case Rectangle(Point2D(x1,y1), Point2D(x2, y2)) => 2*(x2-x1) + 2*(y2-y1)
-    case Circle(Point2D(_,_), r) => 2 * r * Math.PI
+    case Circle(Point2D(_,_), r) => 2*r*Math.PI
     case Square(Point2D(_,_), l) => 4*l
+
+  @tailrec
+  def contains(p: Point2D, s: Shape): Boolean = (p,s) match
+    case (Point2D(x,y), Rectangle(Point2D(x1,y1), Point2D(x2, y2))) => (x >= x1 && y >= y1 && x <= x2 && y <= y2)
+    case (a, Circle(e, r)) => distance(a, e) <= r
+    case (e, Square(Point2D(x1,y1), l)) => contains(e, Rectangle(Point2D(x1,y1),Point2D(x1+l, y1+l)))
+
+
+  def distance(p1: Point2D, p2: Point2D): Double = (p1, p2) match
+    case (Point2D(x1, y1), Point2D(x2, y2)) => Math.sqrt(Math.pow(2, x2 - x1) + Math.pow(2, y2 - y1))
 
 
 
--- test/task4/ShapeTest.scala
+++ test/task4/ShapeTest.scala
@@ -11,3 +11,12 @@ class ShapeTest:
     assertEquals(10, perimeter(Rectangle(Point2D(3,0), Point2D(6,2))))
     assertEquals(2*Math.PI*3.2, perimeter(Circle(Point2D(0,0), 3.2)))
     assertEquals(8, perimeter(Square(Point2D(0,0), 2)))
+
+  @Test def testContains() =
+    assertTrue(contains(Point2D(2,0), Rectangle(Point2D(0,0), Point2D(5,3))))
+    assertFalse(contains(Point2D(7,4), Rectangle(Point2D(0,0), Point2D(5,3))))
+    assertTrue(contains(Point2D(0,3), Circle(Point2D(0,0), 8)))
+    assertTrue(contains(Point2D(1,3), Square(Point2D(0,0), 4)))
+    assertFalse(contains(Point2D(7,7), Square(Point2D(0,0), 4)))
+
+
